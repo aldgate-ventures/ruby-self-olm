9b54a165ac73b4c3bbb371e97ac98d594338b8e2
Tom Bevan <tom.bevan@r3labs.io>
2023-06-22 23:22:12 +0400

    enable PIC for compile and disable PIE for linking

--- ext/self_crypto/extconf.rb
+++ ext/self_crypto/extconf.rb
@@ -1,13 +1,12 @@
 require 'mkmf'
 
-$CFLAGS = " -std=c99"
+$CFLAGS = "-fPIC -std=c99"
+$LDFLAGS = " -no-pie -shared " + $LDFLAGS
 
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
-pkg_config('stdc++')
 pkg_config('self_omemo')
 
-abort "Missing stdc++" unless have_library("stdc++")
 abort "Missing omemo" unless have_library("self_omemo")
 
 create_makefile('self_crypto/self_crypto')
